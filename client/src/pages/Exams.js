0170819188cf4e31293e8b1a45d536945f557d38
Yesenia L <ylopezcr123@gmail.com>
2024-02-08 14:55:57 -0500

    installed chakraui for nav

--- client/src/pages/Exams.js
+++ client/src/pages/Exams.js
@@ -1,22 +1,18 @@
 import React from "react";
 import { useApi } from "../hooks/use-api";
 export default function Home() {
-  //useEffect
-  //render exams
   const { response } = useApi();
-  console.log(response, "wtf is going on?");
+  console.log(response);
   return (
-      <div className="home">
-        <h1> List of exams:</h1>
-        {/* {response && response?.exams[3].patientId} */}
-
+    <div className="home">
+      <h1> List of exams:</h1>
+      {/* {response && response?.exams[3].patientId} */}
+      {/* 
         {response && response?.exams.map((exam) => (
          <div>
          <h2>{exam.exams.patientId}</h2>
-        {/* <p>Age: {exam.age}</p> */}
         </div>
-        ))}
-      </div>
-
+        ))} */}
+    </div>
   );
 }
