823a14be8b7ad05b77d53c6465d9444a1296587a
Craig Tyler Celestin <crgcelestin@gmail.com>
2024-02-08 20:48:44 -0500

    Revert "Navbar Implementation"

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,20 +1,21 @@
-import { Link as ReactRouterLink } from "react-router-dom";
-import { Link as ChakraLink, LinkProps } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-
-    <div className="nav">
-      <ChakraLink as={ReactRouterLink} to="/exams">
-        Logo
-      </ChakraLink>
-      <ChakraLink as={ReactRouterLink} to="/exams">
-        Exams
-      </ChakraLink>
-      <ChakraLink as={ReactRouterLink} to="/admin">
-        Admin
-      </ChakraLink>
-
+    <div className='nav'>
+          <Link to="/exams">
+          <h1>Logo</h1>
+        </Link>
+       <Link to="/exams">
+          <h1>Exams</h1>
+        </Link>
+        <Link to="/admin">
+          <h1>Admin</h1>
+        </Link>
+        {/* Commented out searchbar feature for now - Ben */}
+        {/* <div>
+        <input type="text" />
+        </div> */}
     </div>
-  );
+  )
 }
