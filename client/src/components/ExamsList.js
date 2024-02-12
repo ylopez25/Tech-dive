e651041005f74e6e8686898bd7bc9f1265566416
Craig Tyler Celestin <crgcelestin@gmail.com>
2024-02-12 16:46:33 -0500

    added spinner

--- client/src/components/ExamsList.js
+++ client/src/components/ExamsList.js
@@ -4,37 +4,73 @@ import { Link as ChakraLink, LinkProps } from '@chakra-ui/react'
 
 import React from "react";
 import { useExamContext } from "../context/ExamContext";
+import { Spinner } from '@chakra-ui/react'
 
-export default function ExamsList({ exams }) {
+export default function ExamsList({ loading, exams }) {
 
   const { updateExam } = useExamContext();
+  if (loading) {
+    return (
+      <>
+        <div className="examsList">
+          <TableContainer>
 
+            <Table size="sm" variant="simple" width="100%">
+              <Thead>
+                <Tr>
+                  <Th>Exam ID</Th>
+                  <Th>Patient ID</Th>
+                  <Th>Exams Types</Th>
+                  <Th>Image</Th>
+                  <Th>Key Findings</Th>
+                  <Th>Brixia Score</Th>
+                  <Th isNumeric>Age</Th>
+                  <Th>Sex</Th>
+                  <Th>BMI</Th>
+                  <Th>zipcode</Th>
+                </Tr>
+              </Thead>
+            </Table>
+          </TableContainer>
+        </div>
 
-  return (
-    <div className="examsList">
-      <TableContainer>
-        <Table size="sm" variant="simple" width="100%">
-          <TableCaption>All Exam Records</TableCaption>
-          <Thead>
-            <Tr>
-              <Th>Patient ID</Th>
-              <Th>Exams</Th>
-              <Th>Image</Th>
-              <Th>Key Findings</Th>
-              <Th>Brixia Score</Th>
-              <Th isNumeric>Age</Th>
-              <Th>Sex</Th>
-              <Th>BMI</Th>
-              <Th>zipcode</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {exams.map(
-              (exam, index) =>
-                exam && (
-                  <>
+        <Spinner
+          className="spinner"
+          size="lg"
+        />
+      </>
+    )
+  }
+  if (!loading) {
+    return (
+
+      <div className="examsList">
+        <TableContainer>
+          <Table size="sm" variant="simple" width="100%">
+            <TableCaption>All Exam Records</TableCaption>
+            <Thead>
+              <Tr>
+                <Th>Patient ID</Th>
+                <Th>Exams</Th>
+                <Th>Image</Th>
+                <Th>Key Findings</Th>
+                <Th>Brixia Score</Th>
+                <Th isNumeric>Age</Th>
+                <Th>Sex</Th>
+                <Th>BMI</Th>
+                <Th>zipcode</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {exams.map(
+                (exam, index) =>
+                  exam && (
                     <Tr key={index}>
-                      <Td> {exam.patientId}</Td>
+                      <Td>
+                        <ChakraLink
+
+                        >{exam.patientId}</ChakraLink>
+                      </Td>
                       <Td > <ChakraLink as={ReactRouterLink} color="blue.500" to="/examdetails" onClick={() => updateExam(exam)}>{exam.examId} </ChakraLink></Td>
 
                       <Td>
@@ -48,13 +84,13 @@ export default function ExamsList({ exams }) {
                       <Td>{exam.bmi}</Td>
                       <Td>{exam.zipCode}</Td>
                     </Tr>
-                  </>
-                )
-            )}
-          </Tbody>
-        </Table>
-      </TableContainer>
-    </div>
+                  )
+              )}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </div >
 
-  );
+    );
+  }
 }
--- client/src/pages/Exams.js
+++ client/src/pages/Exams.js
@@ -6,13 +6,16 @@ import { useState, useEffect } from 'react'
 
 export default function Exams() {
     const [exams, setExams] = useState([]);
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
         const fetchExams = async () => {
             try {
+                setLoading(true)
                 const response = await fetch('https://czi-covid-lypkrzry4q-uc.a.run.app/api/exams')
                 const res = await response.json();
                 const exams_data = res["exams"]
+                setLoading(false)
                 setExams(exams_data)
             } catch (e) {
                 console.error(e)
@@ -27,7 +30,7 @@ export default function Exams() {
                 <div className="total">
                     <p>Total: {exams.length}</p>
                 </div>
-                <ExamsList exams={exams} />
+                <ExamsList loading={loading} exams={exams} />
             </div>
         </>
     );
