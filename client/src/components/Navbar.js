a610e1e3ae7644a81ce84f3b1e8b0e6c156d9035
Craig Tyler Celestin <crgcelestin@gmail.com>
2024-02-15 22:23:06 -0500

    going to create new branch to do work

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <div className='nav'>
-          <Link to="/exams">
-          <h1>Logo</h1>
-        </Link>
-       <Link to="/exams">
-          <h1>Exams</h1>
-        </Link>
-        <Link to="/admin">
-          <h1>Admin</h1>
-        </Link>
-        {/* Commented out searchbar feature for now - Ben */}
+      <Link to="/exams">
+        <h1>Logo</h1>
+      </Link>
+      <Link to="/exams">
+        <h1>Exams</h1>
+      </Link>
+      <Link to="/admin">
+        <h1>Admin</h1>
+      </Link>
+      {/* Commented out searchbar feature for now - Ben/}
         {/* <div>
         <input type="text" />
         </div> */}
--- client/src/pages/Exams.js
+++ client/src/pages/Exams.js
@@ -6,20 +6,22 @@ export default function Home() {
   const { response } = useApi();
   console.log(response, "wtf is going on?");
   return (
-      <div className="home">
-        <h1> List of exams:</h1>
-        {/* {response && response?.exams[3].patientId} */}
+    <div className="body">
+      <h1
+        style={{ textAlign: 'center ', padding: '20px' }}
+      > List of exams:</h1>
+      {/* {response && response?.exams[3].patientId} */}
 
-         {/* I commented the mapping feature out for now, it was breaking
+      {/* I commented the mapping feature out for now, it was breaking
          but the Nav Bar is working - Ben */}
-        
-        {/* {response && response?.exams.map((exam) => (
+
+      {/* {response && response?.exams.map((exam) => (
          <div>
          <h2>{exam.exams.patientId}</h2>
         <p>Age: {exam.age}</p>
         </div>
         ))} */}
-      </div>
+    </div>
 
   );
 }
