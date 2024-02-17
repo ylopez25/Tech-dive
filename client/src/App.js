124bb814de821fa00d6f1cb1b610495daa972ed0
KVCobbs <kenyacobbs9001@gmail.com>
2024-02-17 15:29:41 -0500

    dialogue

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,40 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
 import PageNotFound from "./pages/404PageNotFound";
-import './App.css';
-import ExamsList from './components/examsList'
-import LandingPage from './pages/LandingPage';
-import PatientExample from './pages/patientExample'
+import "./App.css";
+import ExamsList from "./components/examsList";
+import LandingPage from "./pages/LandingPage";
+import PatientExample from "./pages/patientExample";
 import Navbar from "./components/Navbar";
-import AdminList from './components/adminList';
-import ExamDetails from './components/examDetail';
-import UpdatePage from './components/adminUpdate';
+import AdminList from "./components/adminList";
+import ExamDetails from "./components/examDetail";
+import UpdatePage from "./components/adminUpdate";
 
 function App() {
-  // const { response } = useApi();
+	// const { response } = useApi();
 
-  return (
-    <ChakraProvider>
-      <div className="App">
-        <BrowserRouter>
-          <Navbar />
-          <div className="pages">
-            <Routes>
-              <Route path="*" element={<PageNotFound />} />
-              <Route
-                path="/exams"
-                element={<ExamsList />}
-              />
-              {/* <Route
+	return (
+		<ChakraProvider>
+			<div className="App">
+				<BrowserRouter>
+					<Navbar />
+					<div className="pages">
+						<Routes>
+							<Route path="*" element={<PageNotFound />} />
+							<Route path="/exams" element={<ExamsList />} />
+							{/* <Route
                 path="/"
                 element={<LandingPage />}
               /> */}
-              <Route
-                path="/admin_list"
-                element={<AdminList />}
-              />
-              <Route
-                path="/examdetails"
-                element={<ExamDetails />}
-              />
-              <Route
-                path="/update_exam"
-                element={<UpdatePage />}
-              />
-            </Routes>
-          </div>
-        </BrowserRouter>
-      </div>
-    </ChakraProvider>
-  );
+							<Route path="/admin/exams" element={<AdminList />} />
+							<Route path="/examdetails" element={<ExamDetails />} />
+							<Route path="/admin/exams/:id/update" element={<UpdatePage />} />
+						</Routes>
+					</div>
+				</BrowserRouter>
+			</div>
+		</ChakraProvider>
+	);
 }
 
 export default App;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,21 +1,21 @@
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  return (
-    <div className='nav'>
-      <Link to="/exams">
-        <h1>Logo</h1>
-      </Link>
-      <Link to="/exams">
-        <h1>Exams</h1>
-      </Link>
-      <Link to="/admin_list">
-        <h1>Admin</h1>
-      </Link>
-      {/* Commented out searchbar feature for now - Ben */}
-      {/* <div>
+	return (
+		<div className="nav">
+			<Link to="/exams">
+				<h1>Logo</h1>
+			</Link>
+			<Link to="/exams">
+				<h1>Exams</h1>
+			</Link>
+			<Link to="/admin/exams">
+				<h1>Admin</h1>
+			</Link>
+			{/* Commented out searchbar feature for now - Ben */}
+			{/* <div>
         <input type="text" />
         </div> */}
-    </div>
-  )
+		</div>
+	);
 }
