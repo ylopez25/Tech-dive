a3d9af588455754fab15cf00d1cfd323e7c374dc
+ <+>
2024-02-18 20:37:12 -0500

    "Patient display"

--- client/src/components/PatientInfo.js
+++ client/src/components/PatientInfo.js
@@ -1,4 +1,18 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 function PatientInfo() {
+
+  const { id } = useParams();
+  const [patient, setPatient] = useState()
+  const [patientExams, setPatientExams] = useState([])
+
+  // useEffect(() => {
+  //   fetch(`/api/exams/${id}`)
+  //     .then(response => response.json())
+  //     .then(data => setPatient(data))
+  //     .catch(error => console.error("Error fetching patient:", error));
+  // }, [id]);
+
   return (
     <>
       <div>
