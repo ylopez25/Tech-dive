c7a1216c5cb91220e706f7e64861133a437a4d0e
Ben Ortiz <bean@Bens-MBP.lan>
2024-02-20 17:16:50 -0500

    fixed typos on issue

--- api/models/examModel.js
+++ api/models/examModel.js
@@ -35,7 +35,7 @@ const examSchema = new Schema({
         type: String,
         required: true
     },
-    brixiaScoress: {
+    brixiaScores: {
         type: String,
         required: true
     },
